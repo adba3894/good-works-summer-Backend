0b501630a0189615eee10255ebdfda18c1e97830
adba3894 <37040637+adba3894@users.noreply.github.com>
2019-03-02 13:53:27 +0200

    Initial commit

---
+++ src/main/java/com/good/works/summer/project/controller/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.good.works.summer.project.controller;
+
+import com.good.works.summer.project.model.Message;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorldController {
+
+    private static final String MESSAGE = "Hello, World!";
+
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Message hello() {
+        Message message = new Message();
+        message.setMessage(MESSAGE);
+        return message;
+    }
+
+}
---
+++ src/main/java/com/good/works/summer/project/model/Message.java
@@ -0,0 +1,15 @@
+package com.good.works.summer.project.model;
+
+public class Message {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
