2e7b4656d13eeeb93abec410bef5b8cef523342c
adba3894 <37040637+adba3894@users.noreply.github.com>
2019-03-05 08:18:44 +0200

    trying to get Lithuanian Cities in heroku
    localhost with h2 database is working perfectly, but I am trying to make it work with heroku postgre database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,3 @@
-CREATE TABLE `city` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8_lithuanian_ci DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_lithuanian_ci;
-
 INSERT INTO `city` (`id`, `name`) VALUES
 	(2,'Vilnius'),
 	(3,'Kaunas'),
