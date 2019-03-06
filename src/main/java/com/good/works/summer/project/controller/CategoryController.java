6fcc4cc3b463e22c9372d9ad8f78c8af73384f7d
adba3894 <37040637+adba3894@users.noreply.github.com>
2019-03-06 09:40:56 +0200

    change

--- src/main/java/com/good/works/summer/project/controller/CategoryController.java
+++ src/main/java/com/good/works/summer/project/controller/CategoryController.java
@@ -4,12 +4,14 @@ package com.good.works.summer.project.controller;
 import com.good.works.summer.project.model.Category;
 import com.good.works.summer.project.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/initialdata")
 public class CategoryController {
--- src/main/java/com/good/works/summer/project/controller/CityController.java
+++ src/main/java/com/good/works/summer/project/controller/CityController.java
@@ -3,12 +3,14 @@ package com.good.works.summer.project.controller;
 import com.good.works.summer.project.model.City;
 import com.good.works.summer.project.service.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/initialdata")
 public class CityController {
