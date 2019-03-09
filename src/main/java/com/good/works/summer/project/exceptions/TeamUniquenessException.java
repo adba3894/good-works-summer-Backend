28f62a5ecbf5757fb5a0c59981346194514fa7c0
Adomas Bazinys <37040637+adba3894@users.noreply.github.com>
2019-03-09 10:40:31 +0200

    Update TeamUniquenessException.java

--- src/main/java/com/good/works/summer/project/exceptions/TeamUniquenessException.java
+++ src/main/java/com/good/works/summer/project/exceptions/TeamUniquenessException.java
@@ -3,7 +3,7 @@ package com.good.works.summer.project.exceptions;
 public class TeamUniquenessException extends Exception{
 
     public TeamUniquenessException(){
-        super("Team with same data already exists!");
+        super("Team with this data already exists!");
     }
 
 }
