40e47782b2a7993c4ce5a30d365a24fe565a460c
adba3894 <37040637+adba3894@users.noreply.github.com>
2019-03-11 13:41:23 +0200

    1st sprint done

--- src/main/java/com/good/works/summer/project/exceptions/UniqueTeamException.java
+++ src/main/java/com/good/works/summer/project/exceptions/UniqueTeamException.java
@@ -1,9 +1,5 @@
 package com.good.works.summer.project.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class UniqueTeamException extends Exception{
 
     public UniqueTeamException(){
