add200cef195d3d19593a5f7bbb171af282c273b
adba3894 <37040637+adba3894@users.noreply.github.com>
2019-03-14 23:33:09 +0200

    trying to implement admin functionality -> it fails for now

--- src/main/java/com/good/works/summer/project/Application.java
+++ src/main/java/com/good/works/summer/project/Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -38,6 +39,10 @@ public class Application {
 		};
 	}
 
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 }
 
---
+++ src/main/java/com/good/works/summer/project/repository/ApplicationUserRepository.java
@@ -0,0 +1,10 @@
+package com.good.works.summer.project.repository;
+
+import com.good.works.summer.project.entities.ApplicationUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApplicationUserRepository extends JpaRepository<ApplicationUser, Long> {
+    ApplicationUser findByUsername(String username);
+}
