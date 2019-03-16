457fee08fbe0d2bb55589916e6bdf551a74016f4
Arvydas <35611633+Arvy1998@users.noreply.github.com>
2019-03-16 21:13:59 +0200

    Update ProjectController.java

--- src/main/java/com/good/works/summer/project/controller/ProjectController.java
+++ src/main/java/com/good/works/summer/project/controller/ProjectController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
