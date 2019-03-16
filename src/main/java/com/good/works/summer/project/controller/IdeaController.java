30854fa957adac517dcba46a8f3a69e2815fe07f
Arvydas <35611633+Arvy1998@users.noreply.github.com>
2019-03-16 21:39:31 +0200

    Update IdeaController.java

--- src/main/java/com/good/works/summer/project/controller/IdeaController.java
+++ src/main/java/com/good/works/summer/project/controller/IdeaController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
