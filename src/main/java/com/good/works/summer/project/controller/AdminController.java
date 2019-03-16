b881735da23ac09ee75af543a79bee6416051fdb
Arvydas <35611633+Arvy1998@users.noreply.github.com>
2019-03-16 21:13:24 +0200

    Update AdminController.java

--- src/main/java/com/good/works/summer/project/controller/AdminController.java
+++ src/main/java/com/good/works/summer/project/controller/AdminController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origin = "*")
 @RestController
 public class AdminController {
 
